66d23473331bd1cc7555aa1b9a240c190db28c54
htmldiv <gzk1215@163.com>
2021-06-30 19:20:33 +0800

    按需引入element+

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import { useElementPlus } from "../src/plugins/element-plus";
+// import ElementPlus from 'element-plus';
+// import 'element-plus/lib/theme-chalk/index.css';
 import Vant from 'vant';
 import 'vant/lib/index.css';
 import App from './App.vue'
@@ -8,7 +9,7 @@ import router from './router';
 
 const app = createApp(App)
 app.use(Vant)
-  .use(ElementPlus)
+  .use(useElementPlus)
   .use(router)
   .mount('#app')
 
